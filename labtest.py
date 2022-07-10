0609e9502f71794ccd553e629486dc78c73d73af
bill <bill@192.168.1.7>
2022-07-11 02:20:31 +0800

    add new demo test code with hsv color space

--- labtest.py
+++ labtest.py
@@ -1,1 +1,27 @@
-from skimage import color
+#load image a.bmp and convert it to lab color space (L*a*b) then convert it to single prcesition floating point without lightness channel  show the result image alongside the original
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+image = cv2.imread('matlab/cc.bmp')
+
+plt.figure()
+# plt.imshow(image)
+# plt.show()
+imageLab = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
+imageLab = image[:,:,1:2]
+#
+plt.imshow(imageLab)
+plt.show()
+#kmeans clustering 2 clusters
+kmeans = KMeans(init='k-means++', n_clusters=3, n_init=10)
+fitImageLab=imageLab.reshape(-1, 1)
+kmeans.fit(fitImageLab)
+P = kmeans.predict(fitImageLab)
+P = P.reshape(imageLab.shape)
+plt.figure()
+plt.imshow(P, cmap='gray')
+plt.show()
+
+
+exit()
--- matlab/demofard.m
+++ matlab/demofard.m
@@ -1,17 +1,16 @@
-% srcPath="Y:\zebra\21-11-07 17-44-18-cam1-zebra\21-11-07 17-44-18\"
-% imagefiles = dir(strcat(srcPath,'*.bmp'));      
-% nfiles = length(imagefiles);    % Number of files found
-% for ii=1:nfiles
-%    currentfilename = imagefiles(ii).name;
-%    path =strcat( srcPath,currentfilename);
-%    currentimage = imread(path);
-% 
-%    mask = segmentImageResize(currentimage)
-%    result = imfuse(currentimage,imresize(mask,10),'blend')
-%    imshow(result)
-%    waitforbuttonpress
-% end
-i = imread("d.bmp")
+srcPath="/Users/bill/Documents/MATLAB/computervision_learning/matlab/fardimg/"
+imagefiles = dir(strcat(srcPath,'*.bmp'));      
+nfiles = length(imagefiles);    % Number of files found
+for ii=1:nfiles
+   currentfilename = imagefiles(ii).name;
+   path =strcat( srcPath,currentfilename);
+   I = imread(path);
+   
+   greenMask=hsvMaskS(I)
+   I1 = I .* uint8(greenMask);
+   grayI=rgb2gray(I1)
+   mask=morph(grayI)
+   nexttile
+   imshowpair(I,mask,"blend")
+end
 
-mask = segmentImageTh(i)
-imshowpair(i,mask,"blend")
